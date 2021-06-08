8915c4b7188b04f5aab9f995147c73045c82ee38
Hatice <haticekaraty@gmail.com>
2021-06-08 15:57:53 -0400

    set sessions controller create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,19 @@ class SessionsController < ApplicationController
         @teacher = Teacher.new
     end
 
-    
+    def create
+        #binding.pry
+        @teacher = Teacher.find_by_email(email: params[:teacher][:email])
+        if @teacher && @teacher.authenticate(params[:teacher][:password])
+            session[:teacher_id] = @teacher.id
+            redirect_to teacher_path(@teacher)
+        else
+            #display an error message saying email or password is not a match!
+            redirect_to login_path
+        end
+    end
+
+    def destroy
+    end
     
 end
