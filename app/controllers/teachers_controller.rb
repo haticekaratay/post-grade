c2b6551192889bd5dee2df8755c518a3ebecabc7
Hatice <haticekaraty@gmail.com>
2021-06-09 15:11:00 -0400

    Create a new teacher with teacher params

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,25 @@
 class TeachersController < ApplicationController
 
+
+    def new
+        @teacher = Teacher.new
+    end
+
+    def create
+        teacher = Teacher.new(teacher_params)
+        if teacher.save
+           session[:teacher_id]= teacher.id
+           redirect_to teacher_path(teacher)
+        else
+           redirect_to signup_path
+        end
+    end
+
     def show
         @teacher = Teacher.find(params[:id])
+    end
+
+    def teacher_params
+        params.require(:teacher).permit(:username, :email,:password)
     end
 end
