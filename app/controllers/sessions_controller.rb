41126201232719f32cda905fb005d85c911e6101
Hatice <haticekaraty@gmail.com>
2021-06-08 23:41:42 -0400

    create with omniauth action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     def create
         #binding.pry
-        @teacher = Teacher.find_by_email(email: params[:teacher][:email])
+        @teacher = Teacher.find_by(email: params[:teacher][:email])
         if @teacher && @teacher.authenticate(params[:teacher][:password])
             session[:teacher_id] = @teacher.id
             redirect_to teacher_path(@teacher)
@@ -18,7 +18,31 @@ class SessionsController < ApplicationController
         end
     end
 
+    def create_with_google_omniauth
+        teacher = Teacher.find_or_create_by(provider: request.env['omniauth.auth']["provider"], uid: request.env['omniauth.auth']["uid"]) do |t|
+            t.username = gmail_username
+            t.email = gmail
+            t.password = "passwordassignedrandomly#*40weqfnhiosd"
+            t.save
+        end
+        if teacher
+            session[:teacher_id] = teacher.id
+            redirect_to teacher_path(teacher)
+        else
+            redirect_to login_path
+        end
+    end
+
     def destroy
+    end
+
+    private
+    def gmail_username
+        request.env['omniauth.auth']['info']['name']
+    end
+
+    def gmail
+        request.env['omniauth.auth']['info']['email']
     end
     
 end
