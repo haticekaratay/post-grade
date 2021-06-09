5271420c3dfd7f9aa46e97a06773c445120c2e95
Hatice <haticekaraty@gmail.com>
2021-06-09 15:11:35 -0400

    validates username and email of teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,7 @@ class Teacher < ApplicationRecord
     has_many :courses
     has_many :students
     has_secure_password
+
+    validates :username, presence: true
+    validates :email, presence: true, uniqueness: true , format: { with:  /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { minimum: 4, maximum: 254 }
 end
