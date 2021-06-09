2b26da095f9d29206b9835f895f22d04c0f90970
Hatice <haticekaraty@gmail.com>
2021-06-09 15:07:01 -0400

    add routes for signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
+  get '/signup' => 'teachers#new'
+  post '/signup' => 'teachers#create'
+
   resources :students
   resources :courses
   resources :teachers
