5029fb52e30859097c7a90dfe758e6010074f3b2
Hatice <haticekaraty@gmail.com>
2021-06-11 20:53:34 -0400

    Update association between course and assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,4 @@
 class Assignment < ApplicationRecord
+    belongs_to :course
     has_many :students, through: :student_assignments
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,5 @@ class Course < ApplicationRecord
   belongs_to :teacher
   has_many :student_courses
   has_many :students, through: :student_courses
+  has_many :assignments
 end
