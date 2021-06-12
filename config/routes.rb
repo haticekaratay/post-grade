bc040354c533021574ba59ebb6147a87bb21096f
Hatice <haticekaraty@gmail.com>
2021-06-11 20:52:18 -0400

    add nested route for student index page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
   post '/signup' => 'teachers#create'
 
   resources :students
-  resources :courses
+  resources :courses do 
+     resources :students, only: :index
+  end
   resources :teachers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
