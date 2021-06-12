9bf0a49036bee9fec15365ba487e25d3bc79a0f7
Hatice <haticekaraty@gmail.com>
2021-06-11 21:31:24 -0400

    defines students index action to render index view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,12 @@
 class StudentsController < ApplicationController
+    def index
+        if params[:course_id]
+            @course = Course.find(params[:course_id])
+            @students = @course.students
+            @assignments = @course.assignments
+        end
+        # @students = Course.first.students  # 10 students
+        # @assignments = Course.first.assignments
+    end
+    
 end
