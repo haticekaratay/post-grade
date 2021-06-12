fb572c6f3576db9d4d834068ea7ce6644d71e149
Hatice <haticekaraty@gmail.com>
2021-06-11 21:25:28 -0400

    removes direct relation with student

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,8 +1,6 @@
 class Teacher < ApplicationRecord
     has_many :courses
-    has_many :students
     has_secure_password
-
     validates :username, presence: true
     validates :email, presence: true, uniqueness: true , format: { with:  /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { minimum: 4, maximum: 254 }
 end
