136eb98270163e016e218355ae9ba8d517f190fb
Hatice <haticekaraty@gmail.com>
2021-06-11 21:22:36 -0400

    defines fullname instance method

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,12 @@
 class Student < ApplicationRecord
     has_many :student_courses
     has_many :courses, through: :student_courses
-    belongs_to :teacher
     has_many :student_assignments
     has_many :assignments, through: :student_assignments
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+    
 end
 
