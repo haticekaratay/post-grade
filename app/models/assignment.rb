ce0bbaf444e8353f7069dc91943d582401c8f7ff
Hatice <haticekaraty@gmail.com>
2021-06-12 21:21:32 -0400

    adds validation

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,8 @@
 class Assignment < ApplicationRecord
     belongs_to :course
     has_many :students, through: :student_assignments
+    validates_presence_of :name, :description, :max, :due_date
+    validates :max, numericality: { only_integer: true } 
 
     def number_of_student_assigned
         StudentAssignment.where("assignment_id = ? ", "#{self.id}").count
