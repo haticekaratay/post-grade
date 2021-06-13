dfe7d2b255ca63d279d729bdd3534917c2dc3a60
Hatice <haticekaraty@gmail.com>
2021-06-12 21:23:32 -0400

    adds action to create the form

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -2,4 +2,23 @@ class AssignmentsController < ApplicationController
     def show
         @assignment = Assignment.find(params[:id])
     end
+
+    def new
+        @assignment = Assignment.new
+    end
+
+    def create
+        assignment = Assignment.new(assignment_params)
+        if assignment.save
+            redirect_to assignment_path            
+        else
+            redirect_to new_assignment_path
+        end
+    end
+
+    private
+
+    def assignment_params
+        params.require(:assignment).permit(:name, :description, :max, :due_date)
+    end
 end
