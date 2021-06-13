b3d31dbfc318f362019be0a5b011658baef31d78
Hatice <haticekaraty@gmail.com>
2021-06-13 15:44:33 -0400

    update association for studentassignments

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,8 @@
 class Assignment < ApplicationRecord
     belongs_to :course
+    has_many :student_assignments
     has_many :students, through: :student_assignments
+    
     validates_presence_of :name, :description, :max, :due_date
     validates :max, numericality: { only_integer: true } 
 
