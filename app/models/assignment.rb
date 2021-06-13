262c51ff7eb004420a12524aa3c7e05d03a3db93
Hatice <haticekaraty@gmail.com>
2021-06-12 20:35:17 -0400

    defines instance method to find number of students

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,9 @@
 class Assignment < ApplicationRecord
     belongs_to :course
     has_many :students, through: :student_assignments
+
+    def number_of_student_assigned
+        StudentAssignment.where("assignment_id = ? ", "#{self.id}").count
+    end
+
 end
