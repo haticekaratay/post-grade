8de01ceb30dd70a160333ec9c18d2211629560bf
Hatice <haticekaraty@gmail.com>
2021-06-12 20:33:07 -0400

    add bootstrap sass gem and dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,10 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import "bootstrap"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
 
 Rails.start()
 Turbolinks.start()
