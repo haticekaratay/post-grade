ba593dc4b7ddf9f6ccede8d11db81c320dbcf748
Hatice <haticekaraty@gmail.com>
2021-06-19 23:49:39 -0400

    updates a grade at a time

--- app/controllers/student_assignments_controller.rb
+++ app/controllers/student_assignments_controller.rb
@@ -6,4 +6,25 @@ class StudentAssignmentsController < ApplicationController
         student_id: params[:student_id], assignment_id: params[:id])
     end
 
+    def update
+        #binding.pry
+        @student = Student.find(params[:student_id])
+        gradebook_object = StudentAssignment.find_by(student_id: params[:student_id], assignment_id: params[:id])
+        @assignment = Assignment.find(params[:id])
+       # binding.pry
+        gradebook_object.update(gradebook_params)
+        
+        if gradebook_object.valid? 
+            #binding.pry
+            redirect_to course_students_path(@assignment.course)
+        else
+            render :edit
+        end
+    end
+
+    def gradebook_params
+        params.require(:gradebook_object).permit(:grade)
+        #binding.pry
+    end
+
 end
