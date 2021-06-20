e675d8bd2a8886cea9d40d5735b29cd135d88b1d
Hatice <haticekaraty@gmail.com>
2021-06-19 23:50:33 -0400

    add nested routes for edit and update single grade

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,19 @@ Rails.application.routes.draw do
   post '/signup' => 'teachers#create'
 
   resources :students
+  post "/students/:student_id/assignments/:id" => "student_assignments#update", as: "update_student_grade"
   resources :courses do 
-     resources :students, only: [:index, :edit, :update, :show]
+     resources :students, only: [:index, :edit, :update, :show, :create]
   end
   resources :courses do
-    resources :assignments, only: [:new, :index, :create]
+    resources :assignments, except: :destroy
   end
+  
+  resources :students do
+    resources :student_assignments,  only: [:edit, :update]
+  end
+  
+  
   resources :teachers
   resources :assignments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
