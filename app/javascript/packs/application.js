e9d537d17af82f8bd717747782cc4e19b6498d53
Hatice <haticekaraty@gmail.com>
2021-06-21 18:34:45 -0400

    add chartkick gem and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,13 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
-import "bootstrap"
+
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import "chartkick/chart.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import "bootstrap"
+import "../../assets/stylesheets/application.scss"
