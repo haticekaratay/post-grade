7a914361ee11b3ffc7e3c309340884fd79cc98dc
Hatice <haticekaraty@gmail.com>
2021-06-26 20:43:07 -0400

    define onmiauth method to clean up sessions contr

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,4 +3,12 @@ class Teacher < ApplicationRecord
     has_secure_password
     validates :username, presence: true
     validates :email, presence: true, uniqueness: true , format: { with:  /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i }, length: { minimum: 4, maximum: 254 }
+
+    def self.from_omniauth(response)
+        Teacher.find_or_create_by(provider: response[:provider], uid: response[:uid]) do |t|
+            t.username = response['info']['name']
+            t.email = ['info']['email']
+            t.password = "passwordassignedrandomly#*40weqfnhiosd"
+        end
+    end
 end
