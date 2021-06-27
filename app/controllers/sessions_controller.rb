3dc7aaf8f51bb7875beae49c5b5871d6812cf696
Hatice <haticekaraty@gmail.com>
2021-06-26 20:43:56 -0400

    add alert message and layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    layout "login"
     def home
     end
 
@@ -7,25 +8,18 @@ class SessionsController < ApplicationController
     end
 
     def create
-        #binding.pry
         @teacher = Teacher.find_by(email: params[:teacher][:email])
         if @teacher && @teacher.authenticate(params[:teacher][:password])
             session[:teacher_id] = @teacher.id
             redirect_to teacher_path(@teacher)
         else
-            #display an error message saying email or password is not a match!
-            redirect_to login_path
+            redirect_to login_path , alert: 'Invalid Login!'
         end
     end
 
     def create_with_google_omniauth
-        teacher = Teacher.find_or_create_by(provider: request.env['omniauth.auth']["provider"], uid: request.env['omniauth.auth']["uid"]) do |t|
-            t.username = gmail_username
-            t.email = gmail
-            t.password = "passwordassignedrandomly#*40weqfnhiosd"
-            t.save
-        end
-        if teacher
+        teacher = Teacher.from_omniauth(request.env['omniauth.auth'])
+        if teacher.valid?
             session[:teacher_id] = teacher.id
             redirect_to teacher_path(teacher)
         else
@@ -35,16 +29,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session.delete(:teacher_id)
-        redirect_to root_path
+        redirect_to login_path , alert: "Successfully Logged Out!"
     end
 
-    private
-    def gmail_username
-        request.env['omniauth.auth']['info']['name']
-    end
-
-    def gmail
-        request.env['omniauth.auth']['info']['email']
-    end
-    
 end
