77528ece75474909fc1820eae210f7d37557d5f6
Hatice <haticekaraty@gmail.com>
2021-06-28 18:04:06 -0400

    redirect unauthorized users to their own show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
         redirect_to login_path, alert: "You must login first" if !logged_in?
     end
 
+    def redirect_if_not_authorized_to_view
+        if current_teacher
+            if !current_teacher.course_ids.include?(params[:course_id].to_i) || @course.teacher_id != current_teacher.id || @assignment.course.teacher_id != current_teacher.id || @assignments.first.teacher_id != current_teacher.id 
+                redirect_to teacher_path(current_teacher), alert: "You are not allowed to view this page"
+            end
+        end
+    end
+
 end
