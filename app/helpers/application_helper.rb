7ce07b2a9a27d96cd2bcb25c175ee17909168f08
Hatice <haticekaraty@gmail.com>
2021-06-28 17:56:26 -0400

    redirect users if they are not logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
     def current_teacher
         @current_teacher ||= Teacher.find_by_id(session[:teacher_id]) if session[:teacher_id]
     end
+
+    def logged_in?
+        !!current_teacher
+    end
+    
+    def redirect_if_not_logged_in
+        redirect_to login_path, alert: "You must login first" if !logged_in?
+    end
+
 end
