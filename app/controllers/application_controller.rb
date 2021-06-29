00ddae1f44e465d9a46130ec12c75ca68fe0861e
Hatice <haticekaraty@gmail.com>
2021-06-29 00:28:12 -0400

    Refactor authorization method in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    include ApplicationHelper
+    def logged_in?
+        !!current_teacher
+    end
+    
+    def redirect_if_not_logged_in
+        redirect_to login_path, alert: "You must login first" if !logged_in?
+    end
+
+    def is_authorized?
+        if params[:course_id]
+            current_teacher.id == Course.find(params[:course_id]).teacher_id
+        elsif params[:id]
+            current_teacher.id == params[:id].to_i
+        end
+    end
+
+    def redirect_if_not_authorized_to_view
+        if !is_authorized?
+            redirect_to teacher_path(current_teacher), alert: "You are not allowed to view this page!!"
+        end  
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,20 +3,4 @@ module ApplicationHelper
         @current_teacher ||= Teacher.find_by_id(session[:teacher_id]) if session[:teacher_id]
     end
 
-    def logged_in?
-        !!current_teacher
-    end
-    
-    def redirect_if_not_logged_in
-        redirect_to login_path, alert: "You must login first" if !logged_in?
-    end
-
-    def redirect_if_not_authorized_to_view
-        if current_teacher
-            if !current_teacher.course_ids.include?(params[:course_id].to_i) || @course.teacher_id != current_teacher.id || @assignment.course.teacher_id != current_teacher.id || @assignments.first.teacher_id != current_teacher.id 
-                redirect_to teacher_path(current_teacher), alert: "You are not allowed to view this page"
-            end
-        end
-    end
-
 end
