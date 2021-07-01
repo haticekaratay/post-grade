f9244bc27720243e7f3c1da898e4f662ef5c124b
Hatice <haticekaraty@gmail.com>
2021-07-01 18:28:22 -0400

    Render json data to display graph

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,6 +1,20 @@
 class ChartsController < ApplicationController
+    before_action :redirect_if_not_logged_in
+
     def statistics_json
-        students = Student.all
-        render json: students.group(:gender).count
+        hash = StudentCourse.group(:course_id).count
+        array =  hash.map do |k,v| 
+            if find_course(k).teacher_id == current_teacher.id
+                [find_course(k).name, v]
+            end
+        end
+        render json: array.to_h
     end
+
+    private
+
+    def find_course(id)
+        course = Course.find(id)
+    end
+
 end
